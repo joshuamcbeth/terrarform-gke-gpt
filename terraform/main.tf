dd240071135eb6431a51e8b7f59364c922c3f61d
Joshua McBeth <joshua.mcbeth@gmail.com>
2025-10-18 19:30:47 -0400

    Add github service account as writer on artifact registry

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,3 +98,10 @@ resource "google_project_iam_member" "gke_node_sa_role" {
   role    = "roles/container.nodeServiceAccount"
   member  = "serviceAccount:${google_service_account.gke_node_sa.email}"
 }
+resource "google_artifact_registry_repository_iam_member" "terraform_sa_writer" {
+  project    = var.project_id
+  location   = "us-central1"
+  repository = "gpt"
+  role       = "roles/artifactregistry.writer"
+  member     = "serviceAccount:terraform-gke-gpt-github@${var.project_id}.iam.gserviceaccount.com"
+}
