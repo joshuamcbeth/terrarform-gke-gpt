b56e8fdecc8433d2636e1022e6ff378caca2d133
Joshua McBeth <joshua.mcbeth@gmail.com>
2025-10-18 20:58:25 -0400

    Add artifactregistry.reader for node service account

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,6 +98,11 @@ resource "google_project_iam_member" "gke_node_sa_role" {
   role    = "roles/container.nodeServiceAccount"
   member  = "serviceAccount:${google_service_account.gke_node_sa.email}"
 }
+resource "google_project_iam_member" "gke_node_sa_artifact_registry" {
+  project = var.project_id
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_service_account.gke_node_sa.email}"
+}
 resource "google_artifact_registry_repository_iam_member" "terraform_sa_writer" {
   project    = var.project_id
   location   = "us-central1"
